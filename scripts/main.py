fb9a62afed0fcfdba5632168bc70f560fa42080d
LorenzoSacco02 <lollipopsacco@gmail.com>
2024-04-27 17:40:26 +0200

    adding machines in the lab and other attempts

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,9 @@ from Kathara.model.Lab import Lab
 from Kathara.manager.Kathara import Kathara
 import bgp_ls_vis.proto as proto
 import bgp_ls_vis.graphing as graphing
+import networkx as nx
+from fs import open_fs
+from fs.copy import copy_fs
 
 # Create lab
 lab = Lab("18_nodes_twin")
@@ -12,9 +15,30 @@ rpc = proto.GoBGPQueryWrapper(connect=False)
 # Calling get_lsdb with param `filename` set will trigger loading from file instead of RPC
 first_example_dir = r"C:/Users/lolli/Desktop/kathara_digital_twin/dumps"
 lsdb = rpc.get_lsdb(filename=fr"{first_example_dir}/18-node-isis-w-bcast-segment.yaml")
-graph = graphing.build_nx_from_lsdb(lsdb)
 
-print(graph)
+# Create a graph from the lsdb
+graph = graphing.build_nx_from_lsdb(lsdb).to_undirected()
+#graphing.draw_pyplot_graph(graph)
+
+routers = []
+
+for node in graph.nodes(data=True):
+    if node[1]["lsa"]!=None:
+        routers.append(lab.new_machine(f"{node[0].lower()}", **{"image": "kathara/frr"}))
+        print(f"aggiunto {node[0]} al lab")    
+        neig = [n for n in graph.neighbors(node[0])]
+        print(neig)
+        break
+
+
+#fs = open_fs(fr"osfs://C:\Users\lolli\Desktop\kathara_digital_twin\lab")
+#copy_fs(lab.fs, fs)
+
+
+"""for edge in graph.edges(data=True):
+    if edge[2]["lsa"]["linkDescriptor"]:
+        print(f"{edge[:2]}", edge[2]["lsa"]["linkDescriptor"])
+"""
 
 # Deploy lab
 #Kathara.get_instance().deploy_lab(lab=lab)
